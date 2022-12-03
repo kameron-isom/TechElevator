46e7e15a007cc2d44eaa0d6be4f896111e7b8321
Rebecca Sullivan <rebecca.sullivan@techelevator.com>
2022-12-03 09:15:33 -0700

    Add lecture-final/ for week-1

---
+++ java/module-1/week-1/REVIEW/lecture-final/src/main/java/com/techelevator/Example2.java
@@ -0,0 +1,33 @@
+package com.techelevator;
+
+import java.util.Random;
+
+public class Example2 {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(10); //picks a number from 0-9
+        System.out.println("Random number is: " + randomNumber);
+
+        /*
+		For each of the print statements below, finish the description of when the boolean expression will
+		evaluate to true.
+		*/
+        System.out.print("Random number is more than 5: ");
+        System.out.println(randomNumber > 5);
+
+        System.out.print("Random number is in the range 2-7: ");
+        System.out.println(randomNumber >= 2 && randomNumber <= 7);
+
+        System.out.print("Random number isn't 2, 4, or 6: ");
+        System.out.println(!(randomNumber == 2 || randomNumber == 4 || randomNumber == 6));
+
+        System.out.print("Random number is divisible by 2 or 3, but not both: ");
+        System.out.println(randomNumber % 2 == 0 ^ randomNumber % 3 == 0);
+
+        System.out.print("Random number divides 10 evenly: ");
+        System.out.println(randomNumber > 0 && 10 % randomNumber == 0);
+    }
+
+}
