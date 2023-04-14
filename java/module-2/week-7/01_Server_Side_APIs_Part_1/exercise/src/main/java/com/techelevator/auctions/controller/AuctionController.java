7da7e53016fead7e2df8f1db849aeff4a76a9b32
Kameron Isom <isomkameron@gmail.com>
2023-04-13 18:34:59 -0700

    week 7

--- java/module-2/week-7/01_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ java/module-2/week-7/01_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,11 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
@@ -13,5 +17,29 @@ public class AuctionController {
     public AuctionController() {
         this.dao = new MemoryAuctionDao();
     }
+
+    @RequestMapping(  method = RequestMethod.GET)
+    public List<Auction> list (@RequestParam (defaultValue = "")String title_like){
+
+        if(!title_like.isEmpty()){
+            return dao.searchByTitle(title_like);
+        }
+//        if (!title_like.isEmpty()){
+//           return dao.searchByTitle(title_like);
+//        }
+        String x = title_like;
+        return dao.list();
+    }
+
+    @RequestMapping(path= "/{id}", method = RequestMethod.GET)
+    public Auction get(@PathVariable int id){
+        return dao.get(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Auction create(Auction auction) {
+        return dao.create(auction);
+    }
+
 
 }
