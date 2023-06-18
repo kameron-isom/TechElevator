2c5864a19ea59de19f8fa55d4ab64190075527e8
Chaly Perales <isomkameron@gmai.com>
2023-06-18 09:59:22 -0600

    mid mod

--- java/module-3/week-5/mid-module-project/script/app.js
+++ java/module-3/week-5/mid-module-project/script/app.js
@@ -2,45 +2,6 @@
     app.js
 
 */
-<<<<<<< HEAD
-function createCards(product){
-const card = document.createElement('article');
-card.classList.add('.product-card')
-
-//create sku
-let sku= document.createElement('div');
-sku.classList.add('.sku');
-sku.innerText=product.productSku
-card.appendChild(sku);
-
-// create name
-let name= document.createElement('div');
-name.classList.add('.name');
-name.innerText=product.name;
-card.appendChild(name);
-name.setAttribute("data-id", product.productId)
-
-// create price
-let price= document.createElement('div');
-price.classList.add('.price');
-price.innerText=currency(product.price);
-card.appendChild(price);
-
-// create image
-let image= document.createElement('div');
-image.classList.add('.product-image');
-image.src=product.imageName;
-card.appendChild(image);
-
-//create shopping cart
-let shoppingCartIcon = document.createElement('div');
-shoppingCartIcon.classList.add('.cart');
-shoppingCartIcon.innerText='<i class="fa-solid fa-cart-plus icon action" title="Add item to cart"></i>'
-card.appendChild(shoppingCartIcon);
-shoppingCartIcon.setAttribute("data-id", product.productId);
-
-return card;
-=======
 let product=productService.getProducts();
 
 // create cards
@@ -102,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     displayCards();
     addToCart();
     displayDescription();
-    ProductSearch(searchTerm);
+   
     
 
 // alert with description
@@ -124,35 +85,19 @@ document.addEventListener('DOMContentLoaded', () =>{
       });
   
   });
->>>>>>> 57fcb4b964c8b78ae79b2607928aec144f8a32d1
 }
-
-function displayCards(){
-    const section= document.getElementById('product-cards');
-      productService.getProducts.forEach((item)=>{
-       section.appendChild(createCards(item));
+  // display string after clicking on cart
+  function addToCart(){
+    const carts = document.querySelectorAll('.cart');
+    carts.forEach((cart)=>{
+      cart.addEventListener('click',() => {
+          alert("item added to cart!")
+      });
     });
-    
-}
+  }
 
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', () =>{
-    createCards();
-    displayCards();
+    
 
-    const clickName= document.querySelectorAll('.product-name.action');
-    clickName.forEach((productName)=>{
-      productName=Number(productName.getAttribute("data-id"));
-      productName.addEventListener('click',window.alert(product.description))
-    });
-    
-    
-    const shoppingCart = document.querySelectorAll('cart');
-    shoppingCart.forEach((cartItem)=>{
-        shoppingCart=Number(cartItem.getAttribute("data-it"));
-        shoppingCart.addEventListener('click', window.alert("Item has been added to cart"))
-    })
-=======
   //product search
   function ProductSearch(searchTerm){
     const search= document.getElementById('search-tool')
@@ -167,7 +112,6 @@ document.addEventListener('DOMContentLoaded', () =>{
     displayCards(productService.getProducts());
 }
 
->>>>>>> 57fcb4b964c8b78ae79b2607928aec144f8a32d1
     });
   
 
@@ -179,4 +123,5 @@ document.addEventListener('DOMContentLoaded', () =>{
            }
            return getProducts;
         }))
-        };
+        }
+  
